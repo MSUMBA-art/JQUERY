57f973144f0e2d7dc9c06554a91461556b048556
ALEXANDER <msumba83@gmail.com>
2021-10-21 06:56:47 +0200

    jquery

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+//document.querySelector("h1");
 
+//we can do this bellow is the same as above
+//$("h1").addClass("big-title margin");
+
+//$("button").css("color", "blue");
+
+//selecting text using jquery
+$("h1").text("GoodByee")
+
+$("button").text("Don't click me")
