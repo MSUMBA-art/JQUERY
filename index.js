61956c0c40401a5aa963e256c4db9697c446f93e
ALEXANDER <msumba83@gmail.com>
2021-10-22 21:58:34 +0200

    jquery

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 //document.querySelector("h1");
 
 //we can do this bellow is the same as above
-//$("h1").addClass("big-title margin");
+$("h1").addClass("big-title margin");
 
 //$("button").css("color", "blue");
 
 //selecting text using jquery
-$("h1").text("GoodByee")
+//$("h1").text("GoodByee")
 
-$("button").text("Don't click me")
+//$("button").text("Don't click me");
+//$("button").html("<em>Don't click me</em>");
+
+//SELECTING ATTRIBUTES
+
+//$("img").attr("src");
+//$("a").attr("href", "https://loveyourcity.netlify.app/");
+
+//ADDING EVENT LISTENERS USING JQUERY
+// $("h1").click(function() {
+//     $("h1").css("color", "purple");
+// })
+
+//ADDING EVENT LISTENERS TO ALL BUTTONS USing FOR LOOP IN Javascrpt
+// for (let i = 0; i < 5; i++) {
+//     document.querySelectorAll("button")
+//     [i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "purple";
+//     }); 
+// }
+
+//ADDING EVENT LISTENERS TO ALL BUTTONS IN Jquery
+$("button").click(function () {
+    $("h1").css("color", "green");
+})
+
+//targeting input in jq
+// $(document).keypress(function(event) {
+//     // console.log(event.key);
+//     $("h1").text(event.key);
+// })
+
+//same
+
+$(document).on("mouseover", function() {
+    $("h1").css("color", "blue")
+})
+
